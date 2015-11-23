ae9195d2e284a7fb432eacabe04e07861bc89ff0
Daniel Clarke <yogfan800@gmail.com>
2015-11-23 14:56:14 -0500

    should make stars like crazy

--- stars/Form1.cs
+++ stars/Form1.cs
@@ -10,11 +10,38 @@ using System.Windows.Forms;
 
 namespace stars
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            starfield(100);
+        }
+
+        public void starfield(int length)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            Random randNum = new Random();
+
+            int red, green, blue;
+            red = randNum.Next(1, 256);
+            green = randNum.Next(1, 256);
+            blue = randNum.Next(1, 256);
+            int x, y;
+            x = randNum.Next(1, this.Width);
+            y = randNum.Next(1, this.Width);
+
+            SolidBrush drawBrush = new SolidBrush(Color.FromArgb(red, green, blue, 0));
+            for (int counter = 0; counter <= length; counter++)
+            {
+               formGraphics.FillEllipse(drawBrush, x, y, 25, 25);
+            }
+        }
+
     }
 }
